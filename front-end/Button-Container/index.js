1e64dcbb4b8cd06ed109810ca5a8b728fb97ba13
Kasuhira <hheflerdevelopment@gmail.com>
2019-11-05 19:49:02 -0300

    Added Button Container

---
+++ front-end/Button-Container/index.js
@@ -0,0 +1,16 @@
+var btnFive = document.getElementById("btn5");
+
+var btnOne  = document.getelementById('btn1').innerHTML ;
+var btnTwo  = document.getelementById('btn2').innerHTML ;
+var btnThree= document.getelementById('btn3').innerHTML ;
+var btnFour = document.getelementById('btn4').innerHTML ;
+var btnSix  = document.getelementById('btn6').innerHTML ;
+var btnSeven= document.getelementById('btn7').innerHTML ;
+var btnEight= document.getelementById('btn8').innerHTML ;
+var btnNine = document.getelementById('btn9').innerHTML ;
+
+btnFive.onclick = function() {
+
+    alert("button Works");
+
+};
